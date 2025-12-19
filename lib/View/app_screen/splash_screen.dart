3f2b3f497c3da47ee93c0f278ed509e403b92b9c
Saurabh Kharade <saurabhkharade2@gmail.com>
2025-12-19 22:49:46 +0530

    setup project

---
+++ lib/View/app_screen/splash_screen.dart
@@ -0,0 +1,51 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+import '../../Dashboard/dashboard.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    Timer(Duration(seconds: 10), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const HomeScreen()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.emergency, size: 100, color: Colors.white),
+            SizedBox(height: 20),
+            Text(
+              "Medi Slot",
+              style: TextStyle(
+                fontSize: 24,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
