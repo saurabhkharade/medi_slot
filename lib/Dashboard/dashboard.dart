a4bd0532e590116ff17b2998af992e66194a8b85
Saurabh Kharade <saurabhkharade2@gmail.com>
2025-12-20 19:09:22 +0530

    dashboard setup

--- lib/Dashboard/dashboard.dart
+++ lib/Dashboard/dashboard.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:medi_slot/View/Widgete/common_button.dart';
-import 'package:medi_slot/View/Widgete/common_textfield.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -25,61 +23,92 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
       ),
       body: SafeArea(
-        child: Card(
-          elevation: 8,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(25),
-          ),
-          margin: EdgeInsets.all(20),
-          color: Colors.white,
-          child: InkWell(
-            borderRadius: BorderRadius.circular(25),
-            onTap: () {
-              // TODO: Navigate to Appointment Screen
-            },
-            child: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 40),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  const Icon(
-                    Icons.local_hospital,
-                    size: 50,
-                    color: Colors.blue,
-                  ),
-                  const SizedBox(height: 15),
-                  const Text(
-                    "Doctor Appointment",
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.w600,
-                      color: Colors.black87,
+        child: Column(
+          children: [
+
+            SizedBox(
+              width: double.infinity,
+              height: 250,
+              child: Card(
+                elevation: 8,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(25),
+                ),
+                margin: EdgeInsets.all(20),
+                color: Colors.white,
+                child: InkWell(
+                  borderRadius: BorderRadius.circular(25),
+                  onTap: () {
+                    // TODO: Navigate to Appointment Screen
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 30,
+                      horizontal: 40,
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        const Icon(
+                          Icons.local_hospital,
+                          size: 50,
+                          color: Colors.blue,
+                        ),
+                        const SizedBox(height: 15),
+                        const Text(
+                          "Doctor Appointment",
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w600,
+                            color: Colors.black87,
+                          ),
+                        ),
+                      ],
                     ),
                   ),
-                  SizedBox(height: 25),
-
-                  CommonButton(
-                    text: "Login",
-                    onPressed: () {
-                      print("Button Clicked");
-                    },
-                  ),
-
-                  SizedBox(height: 25),
-
-                  CommonTextField(
-                    textEditingController: emailController,
-                    hintText: "Enter Email",
-                    labelText: "Email",
-                    textInputType: TextInputType.emailAddress,
-                    isPassword: true,
-                    prefixIcon: const Icon(Icons.password),
-                    maxline: 1,
-                  ),
-                ],
+                ),
               ),
             ),
-          ),
+
+            SizedBox(
+              height: 160, // 👈 MUST give height
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: 6,
+                itemBuilder: (context, index) {
+                  return Container(
+                    width: 150,
+                    margin: const EdgeInsets.symmetric(horizontal: 10),
+                    child: Card(
+                      elevation: 6,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: InkWell(
+                        borderRadius: BorderRadius.circular(20),
+                        onTap: () {},
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(Icons.local_hospital,
+                                size: 40, color: Colors.blue),
+                            const SizedBox(height: 10),
+                            Text(
+                              "Doctor ${index + 1}",
+                              style: const TextStyle(
+                                  fontWeight: FontWeight.w600),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            )
+
+          ],
         ),
       ),
     );
